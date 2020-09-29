6f8e6562a484511b3fd278eb86f28d2719eb30de
my <lin26618@gmail.com>
2020-09-29 14:11:56 +0300

    [p

--- src/Work.java
+++ src/Work.java
@@ -3,5 +3,6 @@ public class Work {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("rabotaet");
+        System.out.println();
     }
 }
